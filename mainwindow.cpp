7f0253436e6c40afe67dcb93c28f6319b3da1dfa
pinguX <pinguX@yandex.com.tr>
2023-10-16 20:06:07 -0400

    buttons, settings ...

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,15 +7,15 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    QAction *settingAction = ui->menuBar->addAction("Ayarlar");
+/*  QAction *settingAction = ui->menuBar->addAction("Ayarlar");
     connect(settingAction, SIGNAL(triggered()), this, SLOT(ayarlar()));
-
+*/
     //veritabanı ekle
 
 
     if(QDir(yol).exists() || QDir(yol2).exists())
     {
-        ui->pushButton_3->setText("Ayır");
+        ui->mount_button->setText("Ayır");
         ui->textBrowser->append("Bağlı");
     }
 }
@@ -25,7 +25,7 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_pushButton_3_clicked()
+void MainWindow::on_mount_button_clicked()
 {
     QProcess *prosess=new QProcess;
     if(prosess)
@@ -33,13 +33,13 @@ void MainWindow::on_pushButton_3_clicked()
         prosess->setEnvironment( QProcess::systemEnvironment());
         prosess->setProcessChannelMode( QProcess::MergedChannels);
 
-        if (ui->pushButton_3->text()=="Ayır")
+        if (ui->mount_button->text()=="Ayır")
         {
-            prosess->start("gio", QStringList() << "mount" << "-u" << "ftp://pinguXmini.pinguXnet:2557");
+            prosess->start("gio", QStringList() << "mount" << "-u" << "ftp://pinguXmini.pinguX.aTa:2557");
         }
         else
         {
-            prosess->start("gio", QStringList() << "mount" << "ftp://pinguXmini.pinguXnet:2557");
+            prosess->start("gio", QStringList() << "mount" << "ftp://pinguXmini.pinguX.aTa:2557");
         }
 
         prosess->waitForStarted();
@@ -48,24 +48,28 @@ void MainWindow::on_pushButton_3_clicked()
     }
 }
 
-void MainWindow::on_pushButton_2_clicked()
+void MainWindow::on_sync_button_clicked()
 {
     if(!QDir(yol).exists() && !QDir(yol2).exists())
     {
         ui->textBrowser->append("<span style=\"color:fuchsia;\">Bağlı değil ki</span>");
     }
     else
-    {
+    {        
+        ui->textBrowser->clear();
         QProcess *proses=new QProcess;
         if (proses)
         {
             ui->textBrowser->append("<span style=\"color:deepskyblue;\">###################EŞİTLENİYOR#################</span>");
-            ui->pushButton_3->setDisabled(true);
-            ui->pushButton_2->setDisabled(true);
+            ui->mount_button->setDisabled(true);
+            ui->sync_button->setDisabled(true);
             proses->setEnvironment( QProcess::systemEnvironment());
             proses->setProcessChannelMode( QProcess::MergedChannels);
 
             QDir(yol).exists() ? proses->setWorkingDirectory(yol): proses->setWorkingDirectory(yol2);
+
+/*
+ * WILL BE REMOVED
             //proses->start("sh",QStringList() << "aTa/yedekle.sh");
 
             //veritabanından oku
@@ -86,7 +90,7 @@ void MainWindow::on_pushButton_2_clicked()
                 "Telegram," \
                 "'Voice Recorder',";
             QString dosyalar=dizinler.replace(","," ");
-/*
+
             QString dosyalar = "aTa " \
                                 "Alarms " \
                                 "Android/data/com.mobile.myeye/files/XMEye/videorecord " \
@@ -104,15 +108,26 @@ void MainWindow::on_pushButton_2_clicked()
                                 "Telegram " \
                                 "'Voice Recorder' ";
 */
+
             proses->start("/bin/bash",QStringList() << "-c" <<
 "$(mkdir -p $(xdg-user-dir DOWNLOAD)/pngtel); \
-son=$(cat aTa/.local/share/.sonsync | head -n1); \
+son=$(cat .local/share/.sonsync | head -n1); \
 simdi=$(date +%s); \
 dakika=$(((($simdi-$son)/60)+1)); \
-find "+dosyalar+" \
-! -path '*/.thumbnails/*' -cmin -$(echo $dakika) -exec cp --no-preserve=all --parents -vf {} $(xdg-user-dir DOWNLOAD)/pngtel \\;;\
-date +%s > aTa/.local/share/.sonsync; \
+find . \
+! -path '*/.thumbnails/*' ! -path '*/.cache/*' ! -path './Android/*' \
+-cmin -$(echo $dakika) \
+-exec cp --no-preserve=all --parents -vf {} $(xdg-user-dir DOWNLOAD)/pngtel \\;; \
+date +%s > .local/share/.sonsync; \
 find $(xdg-user-dir DOWNLOAD)/pngtel -type d -empty -delete");
+
+/*
+ * CHANGED LINES
+ * son=$(cat aTa/.local/share/.sonsync | head -n1); \
+ * find "+dosyalar+" \
+ * date +%s > aTa/.local/share/.sonsync; \
+ *
+*/
 
             proses->waitForStarted();
             connect(proses,SIGNAL(readyReadStandardOutput()),this,SLOT(readOut()));
@@ -139,24 +154,26 @@ void MainWindow::readErr()
       ui->textBrowser->append(p->readAllStandardError());
 }
 
-void MainWindow::on_pushButton_clicked()
+void MainWindow::on_phone_button_clicked()
 {
+    /*QProcess::execute("nautilus "+yol);*/
+
+    //QObject *obje;
+    QString program="nautilus";
+    QStringList argumanlar;
+    QProcess *konumAc=new QProcess();
+
     if(QDir(yol).exists())
     {
-        /*QProcess::execute("nautilus "+yol);*/
-
-        //QObject *obje;
-        QString program="nautilus";
-        QStringList argumanlar;
         QDir(yol).exists() ? argumanlar << yol : argumanlar << yol2;
-
         //QProcess *konumAc=new QProcess(obje);
-        QProcess *konumAc=new QProcess();
         konumAc->start(program, argumanlar);
     }
     else
     {
-        ui->textBrowser->append("<span style=\"color:fuchsia;\">Bağlı değil ki</span>");
+        ui->textBrowser->append("<span style=\"color:fuchsia;\">Bağlı değil</span>");
+        argumanlar << QDir::homePath();
+        konumAc->start(program, argumanlar);
     }
 }
 
@@ -166,14 +183,14 @@ void MainWindow::pushButton_text()
     {
         if(QDir(yol).exists() && i==9)
         {
-            ui->pushButton_3->setText("Ayır");
+            ui->mount_button->setText("Ayır");
             ui->textBrowser->append("<span style=\"color:greenyellow;\">####################BAĞLANDI##################</span>");
         }
         else
         {
-            if (ui->pushButton_3->text()=="Ayır" && i==9)
+            if (ui->mount_button->text()=="Ayır" && i==9)
             {
-                ui->pushButton_3->setText("Bağla");
+                ui->mount_button->setText("Bağla");
                 ui->textBrowser->append("<span style=\"color:red;\">#####################AYRILDI###################</span>");
             }
         }
@@ -183,8 +200,8 @@ void MainWindow::pushButton_text()
 void MainWindow::msg()
 {
     ui->textBrowser->append("<span style=\"color:deepskyblue;\">#####################BİTTİ#####################</span>");
-    ui->pushButton_3->setEnabled(true);
-    ui->pushButton_2->setEnabled(true);
+    ui->mount_button->setEnabled(true);
+    ui->sync_button->setEnabled(true);
 }
 
 void MainWindow::ayarlar()
